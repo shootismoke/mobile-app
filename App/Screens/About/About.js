401fb58887e91eb02a4b80fc0b23e4d342f7df28
Amaury Martiny <amaury.martiny@protonmail.com>
2018-11-17 12:47:18 +0100

    Add about text

--- App/Screens/About/About.js
+++ App/Screens/About/About.js
@@ -24,15 +24,17 @@ export class About extends PureComponent {
   handleOpenMarcelo = () =>
     Linking.openURL('https://www.behance.net/marceloscoelho');
 
-  render () {
+  render() {
     const { navigation } = this.props;
     return (
       <ScrollView style={theme.withPadding}>
         <BackButton onClick={navigation.pop} style={styles.backButton} />
 
         <View style={styles.section}>
-          <Text style={styles.h2}>About</Text>
-          <Text style={[theme.text, theme.paragraph]}>
+          <Text style={styles.h2}>
+            How do you calculate the number of cigarettes?
+          </Text>
+          <Text style={theme.text}>
             This app was inspired by Berkeley Earth’s findings about the{' '}
             <Text onPress={this.handleOpenArticle} style={theme.link}>
               equivalence between air pollution and cigarette smoking
@@ -55,13 +57,52 @@ export class About extends PureComponent {
         </View>
 
         <View style={styles.section}>
-          <Text style={styles.h2}>About</Text>
-          <Text style={[theme.text, theme.paragraph]}>HELLO</Text>
+          <Text style={styles.h2}>Where does the data come from?</Text>
+          <Text style={theme.text}>
+            Air quality data comes from{' '}
+            <Text onPress={this.handleOpenAqi} style={theme.link}>
+              WAQI
+            </Text>{' '}
+            in the form of PM2.5 AQI levels which are usually updated every one
+            hour and converted to direct PM2.5 levels by the app.
+          </Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.h2}>
+            Why is the station so far away from my current location?
+          </Text>
+          <Text style={theme.text}>
+            Since stations that measure and communicate Air Quality results
+            every hour are expensive, the data is still limited to
+            well-developed regions and larger cities around the globe. If you
+            are far from a more prominent urban center, results will probably
+            not be so accurate. Chances are that your air is better in that case
+            at least!
+          </Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.h2}>
+            The results are weird or inconsistent with other sources!
+          </Text>
+          <Text style={theme.text}>
+            We have also encountered a few surprising results: large cities with
+            better air than small villages; sudden huge increases in the number
+            of cigarettes; stations of the same town showing significantly
+            different numbers... The fact is air quality depends on several
+            factors such as temperature, pressure, humidity and even wind
+            direction and intensity. If the result seems weird for you, check{' '}
+            <Text onPress={this.handleOpenAqi} style={theme.link}>
+              WAQI
+            </Text>{' '}
+            for more information and history on your station.
+          </Text>
         </View>
 
         <View style={styles.credits}>
           <Text style={styles.h2}>Credits</Text>
-          <Text style={[theme.text, theme.paragraph]}>
+          <Text style={theme.text}>
             Concept &amp; Development by{' '}
             <Text onPress={this.handleOpenAmaury} style={theme.link}>
               Amaury Martiny
@@ -111,7 +152,7 @@ const styles = StyleSheet.create({
   h2: {
     ...theme.title,
     fontSize: 32,
-    lineHeight: 44
+    lineHeight: 36
   },
   section: {
     marginBottom: theme.defaultSpacing
