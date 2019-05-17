fde196d0029d004278e17c558c14c13135c043f3
Amaury Martiny <amaury.martiny@protonmail.com>
2019-05-17 17:24:13 +0200

    Add translations

--- App/localization/index.js
+++ App/localization/index.js
@@ -21,17 +21,24 @@ import i18n from 'i18n-js';
 const en = require('./languages/en');
 const es = require('./languages/es');
 const fr = require('./languages/fr');
+const ptBR = require('./languages/pt-BR');
 
 i18n.fallbacks = true;
 i18n.translations = {
   en,
+  'en-US': en,
+  'en-UK': en,
   es,
-  fr
+  fr,
+  'fr-BE': fr,
+  'fr-CH': fr,
+  'fr-FR': fr,
+  'fr-CA': fr,
+  pt: ptBR,
+  'pt-BR': ptBR
 };
 
-// `Localization.locale` can come in the form of `en-US` sometimes, so we just
-// take the 1st part.
-i18n.locale = (Localization.locale || 'en').split('-')[0];
+i18n.locale = Localization.locale || 'en';
 
 export {
   i18n
