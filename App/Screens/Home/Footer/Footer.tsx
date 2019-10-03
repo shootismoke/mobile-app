3fae7d4c5cf8d65f9b7e4c8a2379f559382c2a0c
James Pearson <james@needcode.co.uk>
2019-10-03 09:11:27 +0100

    Keep the existing text notification on Android

--- App/Screens/Home/Footer/Footer.tsx
+++ App/Screens/Home/Footer/Footer.tsx
@@ -15,7 +15,7 @@
 // along with Sh**t! I Smoke.  If not, see <http://www.gnu.org/licenses/>.
 
 import React, { useContext } from 'react';
-import { StyleSheet, View, ViewProps } from 'react-native';
+import { Platform, Share, StyleSheet, View, ViewProps } from 'react-native';
 import { NavigationInjectedProps } from 'react-navigation';
 
 import { aboutSections } from '../../About';
@@ -28,7 +28,7 @@ import * as theme from '../../../util/theme';
 
 interface FooterProps extends NavigationInjectedProps, ViewProps { }
 
-export function Footer (props: FooterProps) {
+export function Footer(props: FooterProps) {
   const { api } = useContext(ApiContext)!;
   const { currentLocation } = useContext(CurrentLocationContext);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -36,26 +36,37 @@ export function Footer (props: FooterProps) {
 
   const isTooFar = isStationTooFar(currentLocation!, api!);
 
-  function goToAbout () {
+  function goToAbout() {
     track('HOME_SCREEN_ABOUT_CLICK');
     navigation.navigate('About');
   }
 
-  function goToAboutWhySoFar () {
+  function goToAboutWhySoFar() {
     track('HOME_SCREEN_ABOUT_WHY_SO_FAR_CLICK');
     navigation.navigate('About', {
       scrollInto: aboutSections.aboutWhyIsTheStationSoFarTitle
     });
   }
 
-  function goToDetails () {
+  function goToDetails() {
     track('HOME_SCREEN_DETAILS_CLICK');
     navigation.navigate('Details');
   }
 
-  function handleShare () {
+  function handleShare() {
     track('HOME_SCREEN_SHARE_CLICK');
-    props.navigation.navigate('ShareModal');
+
+    // Share doesn't currently support images on Android, so the text version
+    if (Platform.OS === 'ios') {
+      props.navigation.navigate('ShareModal');
+    } else {
+      Share.share({
+	title: i18n.t('home_share_title'),
+	message: i18n.t('home_share_message', {
+	  cigarettes: api!.shootISmoke.cigarettes
+	})
+      });
+    }
   }
 
   const renderBigButton = () => {
@@ -64,10 +75,10 @@ export function Footer (props: FooterProps) {
         {i18n.t('home_btn_why_is_station_so_far').toUpperCase()}
       </Button>
     ) : (
-      <Button onPress={goToDetails}>
-        {i18n.t('home_btn_see_detailed_info').toUpperCase()}
-      </Button>
-    );
+	<Button onPress={goToDetails}>
+	  {i18n.t('home_btn_see_detailed_info').toUpperCase()}
+	</Button>
+      );
   };
 
   const renderSmallButtons = () => {
@@ -78,10 +89,10 @@ export function Footer (props: FooterProps) {
             {i18n.t('home_btn_more_details').toUpperCase()}
           </Button>
         ) : (
-          <Button icon="question-circle" onPress={goToAbout} type="secondary">
-            {i18n.t('home_btn_faq_about').toUpperCase()}
-          </Button>
-        )}
+	    <Button icon="question-circle" onPress={goToAbout} type="secondary">
+	      {i18n.t('home_btn_faq_about').toUpperCase()}
+	    </Button>
+	  )}
         <Button icon="share-alt" onPress={handleShare} type="secondary">
           {i18n.t('home_btn_share').toUpperCase()}
         </Button>
