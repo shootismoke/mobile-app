24f34605e3bacfabd38b199d9acf48179a3d3f2d
Amaury Martiny <amaury.martiny@protonmail.com>
2019-11-02 14:56:28 +0100

    Update user mutation

--- App/stores/util/updateUser.ts
+++ App/stores/util/updateUser.ts
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Sh**t! I Smoke.  If not, see <http://www.gnu.org/licenses/>.
 
-import { CreateHistoryItemInput } from '@shootismoke/graphql';
+import { Notifications, UpdateUserInput } from '@shootismoke/graphql';
 import { gql } from 'apollo-boost';
 import * as C from 'fp-ts/lib/Console';
 import { pipe } from 'fp-ts/lib/pipeable';
@@ -23,35 +23,37 @@ import * as TE from 'fp-ts/lib/TaskEither';
 import { client } from '../../util/apollo';
 import { promiseToTE, sideEffect } from '../../util/fp';
 import { getOrCreateUser } from './createUser';
-import { Api } from './fetchApi';
 
-const CREATE_HISTORY_ITEM = gql`
-  mutation createHistoryItem($input: CreateHistoryItemInput!) {
-    createHistoryItem(input: $input)
+const UPDATE_USER = gql`
+  mutation updateUser($userId: ID!, $input: UpdateUserInput!) {
+    updateUser(userId: $userId, input: $input) {
+      _id
+    }
   }
 `;
 
 /**
- * Get or create a user
+ * Update notification setting
  */
-export function createHistoryItem(api: Api) {
+export function getOrupdateUser(notifications: Notifications) {
   return pipe(
     getOrCreateUser(),
-    TE.map<string, CreateHistoryItemInput>(userId => ({
-      rawPm25: api.shootISmoke.rawPm25,
-      universalId: `${api.shootISmoke.provider}|${api.idx}`,
-      userId
+    TE.map<string, { userId: string; input: UpdateUserInput }>(userId => ({
+      userId,
+      input: {
+        notifications
+      }
     })),
     TE.chain(
-      sideEffect(input =>
-        TE.rightIO(C.log(`<createHistoryItem> - ${JSON.stringify(input)}`))
+      sideEffect(({ input }) =>
+        TE.rightIO(C.log(`<updateUser> - ${JSON.stringify(input)}`))
       )
     ),
-    TE.chain(input =>
+    TE.chain(data =>
       promiseToTE(async () => {
         await client.mutate({
-          mutation: CREATE_HISTORY_ITEM,
-          variables: { input }
+          mutation: UPDATE_USER,
+          variables: data
         });
       })
     )
