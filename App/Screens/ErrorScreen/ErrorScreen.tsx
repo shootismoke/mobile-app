4c2faee32922e78af5dc5f9207e8c8836481d82e
Amaury Martiny <amaury.martiny@protonmail.com>
2019-11-10 12:06:48 +0100

    Fix sending to sentry

--- App/Screens/ErrorScreen/ErrorScreen.tsx
+++ App/Screens/ErrorScreen/ErrorScreen.tsx
@@ -55,10 +55,10 @@ const styles = StyleSheet.create({
 
 // We don't send the following errors to Sentry
 const UNTRACKED_ERRORS = [
-  'Error: Permission to access location was denied',
-  'Error: Location provider is unavailable. Make sure that location services are enabled.',
-  'Error: Location request timed out.',
-  'Error: Location request failed due to unsatisfied device settings.'
+  'Permission to access location was denied',
+  'Location provider is unavailable. Make sure that location services are enabled.',
+  'Location request timed out.',
+  'Location request failed due to unsatisfied device settings.'
 ];
 
 export function ErrorScreen(props: ErrorScreenProps) {
