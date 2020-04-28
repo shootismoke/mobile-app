1c3a40f709a650674d7c8dbe187ce28b834daf5a
Amaury Martiny <amaury.martiny@protonmail.com>
2020-04-28 18:04:46 +0200

    fix: Fix production build (#569)
    * Try
    
    * Fix generate script

--- scripts/generateGoogleServicesJson.js
+++ scripts/generateGoogleServicesJson.js
@@ -3,7 +3,8 @@
 /**
  * This script will take the `SIS_GOOGLE_SERVICES_JSON` string, parse it, and
  * output the parsed object. It is used to create the `google-services.json`
- * file in production.
+ * file in production. Doint JSON stringify+parse just to be sure that the env
+ * variable is correctly set.
  */
 
-console.log(JSON.parse(process.env.SIS_GOOGLE_SERVICES_JSON));
+console.log(JSON.stringify(JSON.parse(process.env.SIS_GOOGLE_SERVICES_JSON)));
