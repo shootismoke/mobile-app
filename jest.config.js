7e50d9889f29f7ad716fdf413345c299e98a4c6e
berto <berto.ort@gmail.com>
2020-10-02 18:16:36 -0600

    Fix yarn run v1.22.4 $ jest App Done in 5.66s. #724

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,23 @@
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
 module.exports = {
+	...tsjPreset,
 	preset: 'jest-expo',
 	moduleFileExtensions: ['js', 'ts', 'tsx'],
 	testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+	moduleNameMapper: {
+		'\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/mocks/fileMock.js',
+	},
 	transform: {
-		'^.+\\.tsx?$': 'ts-jest',
+		...tsjPreset.transform,
+	},
+	transformIgnorePatterns: [
+		'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+	],
+	globals: {
+		'ts-jest': {
+			babelConfig: true,
+		},
 	},
 };
